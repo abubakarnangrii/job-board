1a57ebbbe44912d42b3e281cdf1b098dc234ee26
abubakarnangrii <abubakarnangri@gmail.com>
2024-01-24 03:42:52 +0500

    Completed but some functiong missing

---
+++ src/components/JobBoard/JobBoard.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import JobPosting from '../JobPosting/JobPosting'
+import styles from './JobBoard.module.css'
+
+const ITEM_PER_PAGE =6;
+
+
+function JobBoard(props) {
+    const [items, setItems] = useState([]);
+    const [itemsIds, setItemsIds] = useState(null);
+    const [fetchingDetails, setFetchingDetails] = useState(false);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const fetchItems = async (currPage) => {
+        setCurrentPage(currPage);
+        setFetchingDetails(true);
+
+        let itemsList = itemsIds;
+        if (itemsList === null) {
+            const response = await fetch(`${props.API_ENDPOINT}/jobstories.json`);
+            itemsList = await response.json();
+            setItemsIds(itemsList);
+        }
+
+        const itemIdsForPage = itemsList.slice(
+            currPage*ITEM_PER_PAGE,
+            currPage*ITEM_PER_PAGE+ITEM_PER_PAGE
+        );
+
+        const itemsForPage = await Promise.all(
+            itemIdsForPage.map((itemId) => {
+                fetch(`${props.API_ENDPOINT}/item/${itemId}`).then(res => res.json());
+            })
+        )
+        setItems([...items, ...itemsForPage]);
+        setFetchingDetails(false);
+    }
+
+    useEffect(() => {
+        if (currentPage === 0) {
+            fetchItems(currentPage);
+        }
+    }, [])
+    return (
+        <div className={styles.app}>
+            <h1 className={styles.title}>Hacker News Job Board</h1>
+            {
+                itemsIds=== null || items.length < 1 ?
+                    (<p className={styles.loading}>loading...</p>)
+                    :
+                    (<div>
+                        <div className={styles.items} role='list'>
+                            {items.map((item) => {
+                                return <JobPosting key={items.id} {...item} />
+                            })}
+                        </div>
+                        <button onClick={()=>fetchItems(currentPage+1)}
+                        disabled={fetchingDetails}
+                         className={styles['load-more-button']}>{fetchingDetails ? "Loading..." :"Load more jobs"}</button>
+                    </div>)
+            }
+        </div>
+    );
+}
+
+export default JobBoard;
---
+++ src/components/JobPosting/JobPosting.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './JobPosting.module.css'
+
+function JobPosting({url , title,by , time}) {
+    const formattedTime = new Date(time*1000).toLocaleString();
+  return (
+    <div className={styles.post} role='listitem'>
+        <h2 className={styles.post__title}>
+            <a className={url ? styles.a : styles.inactiveLink} 
+            href={url}
+            rel='noopener'
+            >{title}</a>
+        </h2>
+        <span className={styles.post__metadata}>By {by} - {formattedTime}</span>
+    </div>
+  )
+}
+
+export default JobPosting;
